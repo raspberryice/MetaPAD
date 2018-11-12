97772c9793098d633c7f0731caf0b735d7a9426a
“Serena” <serena8744@gmail.com>
2018-11-11 18:47:33 -0600

    add get_tag_path function to get tagging

--- preprocess_general_types.py
+++ preprocess_general_types.py
@@ -28,7 +28,6 @@ def get_top_level_type(t1):
     ss_list = wn.synsets(t1,pos=wn.NOUN)[:3]   # find all the wordset that the word is in（synsets）
     # print(ss_list)
     while ss_list:
-        # print(ss_list[0])
         s = ss_list.pop(0)
         hyp = s.hypernyms()   # get all the nearest upper tag set
         # print(hyp)
@@ -44,24 +43,29 @@ def get_top_level_type(t1):
     return t
 
 # get the second tag that beyond the current tag of word
-def get_nearest_tag(t1):
+def get_tag_path(t1):
     '''
        t1 is the head word of a noun phrase. Multiple types are allowed in case of ambiguity.
        '''
     t = set()
-    tag_list = wn.synsets(t1,pos=wn.NOUN)[:3]
-    while tag_list:
+    tag_list = wn.synsets(t1,pos=wn.NOUN)[:1]
+    if tag_list:
         s = tag_list.pop(0)  # get the first tag
         hyp = s.hypernyms()
-        if not hyp:
-
-            break
-        elif hyp[0] == wn.synset('entity.n.01'):
-            break
-        else:
-            t.add(hyp[0].lemma_names()[0])
-            break
-    print(t)
+        while hyp:
+            if not hyp:
+                break
+            elif hyp[0] == wn.synset('entity.n.01'):
+                break
+            elif hyp[0]._name in top_level_types:
+                t.add(hyp[0].lemma_names()[0])
+                # print(hyp[0]._name,t)
+                return t
+            else:
+                t.add(hyp[0].lemma_names()[0])
+                hyp = hyp[0].hypernyms()
+    # print(t)
+    t.clear()  # not the noun that we want to consider
     return t
 
 
@@ -75,13 +79,15 @@ def parse_word(filename):
         for i in range(len(words)): # find the noun that has not been marked
             if words[i][0]=='<' or words[i][-1]=='>': # already marked
                 continue
-            tag = nltk.pos_tag(words[i])
-            top = get_top_level_type(words[i])
+            top = get_tag_path(words[i])
             if len(top)>0:
-                top_tag = list(top)[0]
-                new_word = "<"+top_tag+">"+words[i]+"</"+top_tag+">"
+                tag_string = ''
+                for top_tag in reversed(list(top)):
+                    tag_string+=top_tag+'.'
+                tag_string = tag_string.strip('.')
+                new_word = "<"+tag_string+">"+words[i]+"</"+tag_string+">"
                 words[i] = new_word # replace the original word with tagged word
-                # print(new_word)
+                print(new_word)
             new_line+=words[i]+" "
         new_line.strip(' ')
         w.write(new_line+"\n")
@@ -93,5 +99,5 @@ def parse_word(filename):
 
 
 if __name__ == '__main__':
-    # parse_word("input/1_corpus.txt")
-    get_nearest_tag("dog")
+    parse_word("input/1_corpus.txt")
+    get_tag_path("man")
