7c6bc427e509a418b8f1d4805a46eb0788ca8e2c
“Serena” <serena8744@gmail.com>
2018-11-12 00:31:49 -0600

    add get_tag_path function to get tagging

--- preprocess_general_types.py
+++ preprocess_general_types.py
@@ -3,7 +3,9 @@ import nltk
 # nltk.download('wordnet')
 # nltk.download('averaged_perceptron_tagger')
 from nltk.corpus import wordnet as wn
+import copy
 noun_phrase = ['NN','NNS','NNPS','NNP']
+
 top_level_types = {
     'person.n.01': 'person',
     'organization.n.01': 'organization',
@@ -47,7 +49,7 @@ def get_tag_path(t1):
     '''
        t1 is the head word of a noun phrase. Multiple types are allowed in case of ambiguity.
        '''
-    t = set()
+    t = []
     tag_list = wn.synsets(t1,pos=wn.NOUN)[:1]
     if tag_list:
         s = tag_list.pop(0)  # get the first tag
@@ -58,13 +60,13 @@ def get_tag_path(t1):
             elif hyp[0] == wn.synset('entity.n.01'):
                 break
             elif hyp[0]._name in top_level_types:
-                t.add(hyp[0].lemma_names()[0])
+                t.append(hyp[0].lemma_names()[0])
                 # print(hyp[0]._name,t)
                 return t
             else:
-                t.add(hyp[0].lemma_names()[0])
+                t.append(hyp[0].lemma_names()[0])
                 hyp = hyp[0].hypernyms()
-    # print(t)
+    # print("dd",t)
     t.clear()  # not the noun that we want to consider
     return t
 
@@ -72,22 +74,43 @@ def get_tag_path(t1):
 
 def parse_word(filename):
     f = open(filename,"r")
-    w = open("new_corpus.txt","w")
+    w = open("input/new_corpus.txt","w")
     for line in f.readlines():
         new_line = ''
         words = line.strip('\r\n').split()
+
+        # get tag
+        tmp = copy.deepcopy(words)
+        for i in range(len(tmp)):
+            if tmp[i][0] == '<':
+                if len(tmp[i].split('>'))>1:
+                 tmp[i] = tmp[i].split('>')[1]
+            if tmp[i][-1] == '>':
+                tmp[i] = tmp[i].split('<')[0]
+        sen = ''
+        for t in tmp:
+            sen+=t+' '
+        sen.strip()
+        text = nltk.word_tokenize(sen)
+        sen_tagged = nltk.pos_tag(text)
+        # print(sen_tagged)
+
         for i in range(len(words)): # find the noun that has not been marked
             if words[i][0]=='<' or words[i][-1]=='>': # already marked
+                new_line += words[i] + " "
+                continue
+            if sen_tagged[i][1] not in noun_phrase:
+                new_line += words[i] + " "
                 continue
             top = get_tag_path(words[i])
             if len(top)>0:
                 tag_string = ''
-                for top_tag in reversed(list(top)):
+                for top_tag in reversed(top):
                     tag_string+=top_tag+'.'
                 tag_string = tag_string.strip('.')
                 new_word = "<"+tag_string+">"+words[i]+"</"+tag_string+">"
                 words[i] = new_word # replace the original word with tagged word
-                print(new_word)
+                # print(new_word)
             new_line+=words[i]+" "
         new_line.strip(' ')
         w.write(new_line+"\n")
@@ -100,4 +123,5 @@ def parse_word(filename):
 
 if __name__ == '__main__':
     parse_word("input/1_corpus.txt")
-    get_tag_path("man")
+    # get_tag_path("professional")
+    # print(wn.synsets("professional"))
